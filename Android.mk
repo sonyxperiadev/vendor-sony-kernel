54664a0eda62023efff6518cb0383f505582e95b
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2015-04-13 17:02:40 +0200

    add build scripts
    initial scripts to use prebuilt kernel
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,26 @@
-#
-# This empty Android.mk file exists to prevent the build system from
-# automatically including any other Android.mk files under this directory.
-#
+ifeq ($(BUILD_KERNEL),false)
+ifeq ($(filter-out amami aries castor eagle flamingo honami leo scorpion seagull sirius tianchi tianchi_dsds togari,$(TARGET_DEVICE)),)
+
+LOCAL_PATH := $(call my-dir)
+
+
+TARGET_PREBUILT_KERNEL := vendor/sony/kernel/kernel-$(TARGET_DEVICE)
+INSTALLED_KERNEL_TARGET ?= $(PRODUCT_OUT)/kernel
+
+DTB_DIR := $(ANDROID_BUILD_TOP)/vendor/sony/kernel/dtbs
+DTB_FILES := $(shell find -L $(DTB_DIR) -name "*$(TARGET_DEVICE)*.dtb")
+DTB_OUT_DIR := $(PRODUCT_OUT)/dtbs
+DTBS := $(foreach dtb,$(DTB_FILES),$(DTB_OUT_DIR)/$(notdir $(dtb)))
+$(DTB_OUT_DIR)/%.dtb : $(DTB_DIR)/%.dtb | $(ACP)
+	$(hide) @mkdir -p $(DTB_OUT_DIR)
+	$(transform-prebuilt-to-target)
+
+file := $(INSTALLED_KERNEL_TARGET)
+ALL_PREBUILT += $(file)
+$(file) : $(TARGET_PREBUILT_KERNEL) $(DTBS) | $(ACP)
+	$(transform-prebuilt-to-target)
+
+ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
+endif
+
+endif
