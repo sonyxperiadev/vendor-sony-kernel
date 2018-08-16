8a8bfff666f5dd27edb7748d3d75695e4a163a41
Alin Jerpelea <alin.jerpelea@sony.com>
2018-08-16 14:59:24 +0200

    add MKDTIMG and compile DTBO images
    This patch adds suport for compiling dtbo images. Those images are used
    by the new devices.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- build-kernels.sh
+++ build-kernels.sh
@@ -4,10 +4,20 @@ export KERNEL_TMP=$ANDROID_ROOT/out/kernel-tmp
 
 # Cross Compiler
 export CROSS_COMPILE=../../prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+# Mkdtimg tool
+export MKDTIMG=$ANDROID_ROOT/out/host/linux-x86/bin/mkdtimg
 # Build command
 export BUILD="make O=$KERNEL_TMP ARCH=arm64 CROSS_COMPILE=$CROSS_COMPILE -j$(nproc)"
 # Copy prebuilt kernel
 export CP_BLOB="cp $KERNEL_TMP/arch/arm64/boot/Image.gz-dtb $KERNEL_TOP/common-kernel/kernel-dtb"
+
+# Check if mkdtimg tool exists
+if [ ! -f $MKDTIMG ]; then
+    echo "mkdtimg: File not found!"
+    echo "Building mkdtimg"
+    cd  $ANDROID_ROOT/
+    make mkdtimg
+fi
 
 LOIRE="suzu kugo blanc"
 TONE="dora kagura keyaki"
@@ -23,15 +33,20 @@ for platform in $PLATFORMS; do \
 
 case $platform in
 loire)
-    DEVICE=$LOIRE;;
+    DEVICE=$LOIRE;
+    DTBO="false";;
 tone)
-    DEVICE=$TONE;;
+    DEVICE=$TONE;
+    DTBO="false";;
 yoshino)
-    DEVICE=$YOSHINO;;
+    DEVICE=$YOSHINO;
+    DTBO="false";;
 nile)
-    DEVICE=$NILE;;
+    DEVICE=$NILE;
+    DTBO="false";;
 tama)
-    DEVICE=$TAMA;;
+    DEVICE=$TAMA;
+    DTBO="true";;
 esac
 
 for device in $DEVICE; do \
@@ -39,5 +54,8 @@ for device in $DEVICE; do \
     $BUILD aosp_$platform"_"$device\_defconfig
     $BUILD
     $CP_BLOB-$device
+    if [ $DTBO = "true" ]; then
+        $MKDTIMG create $KERNEL_TOP/common-kernel/dtbo-$device\.img `find $KERNEL_TMP/arch/arm64/boot/dts -name "*.dtbo"`
+    fi
 done
 done
