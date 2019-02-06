707dbe34325ca870641f929da297895c49ce4a03
Alin Jerpelea <alin.jerpelea@sony.com>
2019-02-06 10:43:23 +0100

    build-kernels.sh: improve error and messages
    Add more messages so that we can track where we are in the build
    process
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- build-kernels.sh
+++ build-kernels.sh
@@ -53,20 +53,32 @@ esac
 for device in $DEVICE; do \
     rm -rf $KERNEL_TMP
     mkdir $KERNEL_TMP
-    $BUILD aosp_$platform"_"$device\_defconfig
+    echo "================================================="
+    echo "Platform -> ${platform} :: Device -> $device"
+    ret=$(${BUILD} aosp_$platform"_"$device\_defconfig 2>&1);
+    case "$ret" in
+        *"error"*|*"ERROR"*) echo "ERROR: $ret"; exit 1;;
+    esac
 
     echo "The build may take up to 10 minutes. Please be patient ..."
     echo "Building new kernel image ..."
     echo "Loggin to $KERNEL_TMP/build_log_${device}"
     $BUILD >$KERNEL_TMP/build_log_${device} 2>&1;
 
-    $CP_BLOB-$device
+    echo "Copying new kernel image ..."
+    ret=$(${CP_BLOB}-${device} 2>&1);
+    case "$ret" in
+        *"error"*|*"ERROR"*) echo "ERROR: $ret"; exit 1;;
+    esac
     if [ $DTBO = "true" ]; then
         $MKDTIMG create $KERNEL_TOP/common-kernel/dtbo-$device\.img `find $KERNEL_TMP/arch/arm64/boot/dts -name "*.dtbo"`
     fi
 done
 done
 
+echo "================================================="
+echo "Clean up environment"
+echo "Done!"
 unset ANDROID_ROOT
 unset KERNEL_TOP
 unset KERNEL_CFG
